bd33900188403ab2e3f7875c7e8b4f940772232b
marvin hazael <marvinhazael@Air-de-marvin.lan>
2024-02-20 16:03:36 +0100

    first commit

--- Desktop/catwalk-back/api/server.js
+++ Desktop/catwalk-back/api/server.js
@@ -8,6 +8,8 @@ import userRoute from "./routes/user.route.js";
 // import messageRoute from "./routes/message.route.js";
 // import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -22,7 +24,10 @@ const connect = async () => {
   }
 };
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -31,6 +36,13 @@ app.use("/api/users", userRoute);
 // app.use("/api/conversations", conversationRoute);
 // app.use("/api/messages", messageRoute);
 // app.use("/api/reviews", reviewRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+
+  return res.status(errorStatus).send(errorMessage);
+});
 
 app.listen(8800, () => {
   connect();
